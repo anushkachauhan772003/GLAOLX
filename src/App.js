cee35e64c8ec536ca22f2a1673254bf6cf6db365
anushkachauhan772003 <anushkachauhan772003@gmail.com>
2023-11-14 19:53:22 +0530

    14 November 2023

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+    <h1>GLAOLX</h1>
+    <button>Normal button</button>
+    <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
